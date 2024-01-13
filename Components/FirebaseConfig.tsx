4f0caae01b67d86660994995d769fc892eac59b4
oazrealo <yeungyin8833@gmail.com>
2024-01-14 03:40:04 +0800

    Firebase configuration (for login authentication)

---
+++ Components/FirebaseConfig.tsx
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+ apiKey: "AIzaSyD4cWSDWv0WyQy_drlMHMw01i--dNW01j0",
+ authDomain: "Yreactnativeapp-901f1.firebaseapp.com",
+ projectId: "reactnativeapp-901f1",
+ storageBucket: "reactnativeapp-901f1.appspot.com",
+ messagingSenderId: "23009546163",
+ appId: "1:23009546163:android:9564f83d6f7e95ed4e05c6"
+};
+
+export const app = initializeApp(firebaseConfig)
